e5686bf277a80990dc22cca17839020c5e282e88
Philipp Gartz <philipp@gartz.dev>
2026-05-03 12:51:38 +0200

    chore: add GitHub Pages deployment workflow

--- build.ts
+++ build.ts
@@ -1,12 +1,8 @@
-/**
- * This file is need because the tailwind plugin does currently only work with the Bun.build API, but not with the CLI. Once the plugin supports the CLI, this file can be removed and the build script in package.json can be changed to "bun build".
- * https://bun.com/docs/bundler/html-static#build-for-production-2
- */
+import tailwind from "bun-plugin-tailwind";
 
 await Bun.build({
 	entrypoints: ["./index.html"],
 	outdir: "./dist",
 	minify: true,
+	plugins: [tailwind],
 });
-
-export {};
